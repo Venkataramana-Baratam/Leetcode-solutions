43ba4b7ba4f55c335a229e326a116fbc450bd5f7
Venkatarmana-Baratam <venkataramanabaratam123@gmail.com>
2025-03-01 09:09:35 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (29.55%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.py
@@ -1,18 +1,15 @@
 from typing import List
-
 class Solution:
     def applyOperations(self, nums: List[int]) -> List[int]:
-        # Modify elements based on adjacent equality
-        for i in range(len(nums) - 1):  # Fix: stop at len(nums)-2
+        for i in range(len(nums) - 1):
             if nums[i] == nums[i + 1]:
                 nums[i] = nums[i] * 2
                 nums[i + 1] = 0
 
-        # Move non-zero elements to the front
-        j = 0  # Initialize j to 0 to keep track of non-zero positions
+        j = 0
         for i in range(len(nums)):
             if nums[i] != 0:
-                nums[j], nums[i] = nums[i], nums[j]  # Swap non-zero element with zero
-                j += 1  # Move to next position for non-zero elements
+                nums[j], nums[i] = nums[i], nums[j]
+                j += 1
                 
         return nums
