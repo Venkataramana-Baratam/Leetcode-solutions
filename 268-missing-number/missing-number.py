7adf4e7ecdcb13908765b7bc669757a590415f26
Venkatarmana-Baratam <venkataramanabaratam123@gmail.com>
2025-03-03 21:58:27 +0530

    Time: 4 ms (49.85%) | Memory: 18.8 MB (34.79%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,9 +1,8 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        n = len(nums)
-        hash = [0] * (n + 1)
-        for i in range(n):
-            hash[nums[i]] += 1
-        for j in range(n + 1):
-            if hash[j] == 0:
-                return j
+        n=len(nums)
+        sum1=(n*(n+1))/2
+        sum2=0
+        for num in nums:
+            sum2+=num
+        return int(sum1-sum2)
