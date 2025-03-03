7dbed2aa88b8ac6d16c65d72cb751106142ab1f5
Venkatarmana-Baratam <venkataramanabaratam123@gmail.com>
2025-03-03 21:49:04 +0530

    Time: 5 ms (43.59%) | Memory: 18.7 MB (52.54%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -2,8 +2,6 @@ class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n = len(nums)
         hash = [0] * (n + 1)
-        
-        # Fill the hash array based on the nums values
         for i in range(n):
             hash[nums[i]] += 1
         for j in range(n + 1):
