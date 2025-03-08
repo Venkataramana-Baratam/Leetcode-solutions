d07b1b3f96990c92e02141e3206c5e543717d84d
Venkatarmana-Baratam <venkataramanabaratam123@gmail.com>
2025-03-08 13:30:21 +0530

    Time: 8 ms (7.74%) | Memory: 17.6 MB (88.78%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.py
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.py
@@ -1,17 +1,10 @@
 class Solution:
     def minimumRecolors(self, blocks: str, k: int) -> int:
-        mini = float('inf')  # Initialize mini with infinity
-        
-        # Iterate over each sliding window of size k
+        mini = float('inf')  
         for i in range(len(blocks) - k + 1):
-            count = 0  # Reset count for each new window
-            
-            # Count 'W's in the current window
+            count = 0 
             for j in range(i, i + k):
                 if blocks[j] == 'W':
                     count += 1
-            
-            # Update mini after processing the entire window
             mini = min(mini, count)
-        
         return mini
