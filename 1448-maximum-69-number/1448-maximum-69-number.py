c7f7fd083b2cfd16627311de58ea6e1a628ecc7b
Venkataramana Baratam <venkataramanabaratam123@gmail.com>
2025-08-16 09:44:04 +0530

    Time: 0 ms (100%), Space: 17.7 MB (76.77%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.py
+++ 1448-maximum-69-number/1448-maximum-69-number.py
@@ -1,14 +1,3 @@
 class Solution:
     def maximum69Number (self, num: int) -> int:
-        ans = []
-        temp = str(num)
-        for i in temp:
-            ans.append(i)
-        maxi = 0
-        res = ""
-        for i in range(len(ans)):
-            if ans[i]=='6':
-                ans[i] = '9'
-                break
-        res = ''.join(ans)
-        return int(res)
+        return int(str(num).replace('6', '9', 1))
