f4ce1f130b6b9f3a44b72d0d63c469f47911d81b
Venkataramana Baratam <venkataramanabaratam123@gmail.com>
2025-08-20 18:32:04 +0530

    Time: 42 ms (30.41%), Space: 18.8 MB (25.59%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.py
@@ -0,0 +1,9 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        max_index =0
+        n = len(nums)
+        for i in range(n):
+            if i>max_index:
+                return False
+            max_index = max(max_index,i+nums[i])
+        return True 
