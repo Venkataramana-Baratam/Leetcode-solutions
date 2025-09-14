af65d7a8f0f24f5eeec54b0748fc0d0bea22c8cc
Venkataramana Baratam <venkataramanabaratam123@gmail.com>
2025-09-14 07:27:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 4011-smallest-absent-positive-greater-than-average/4011-smallest-absent-positive-greater-than-average.py
+++ 4011-smallest-absent-positive-greater-than-average/4011-smallest-absent-positive-greater-than-average.py
@@ -3,6 +3,8 @@ class Solution:
         n = len(nums)
         arr_sum = sum(nums)
         avg = arr_sum/n
-        for i in range(1,150):
-            if i>avg and i not in nums:
+        i = int(avg)+1
+        while True:
+            if i not in nums:
                 return i
+            i+=1
