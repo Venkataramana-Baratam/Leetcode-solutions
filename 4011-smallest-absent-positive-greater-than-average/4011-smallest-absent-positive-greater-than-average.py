c6f9c545e1d37dcd8df30a44c8d144e8a9897f43
Venkataramana Baratam <venkataramanabaratam123@gmail.com>
2025-09-14 07:30:01 +0530

    Time: 7 ms (0%), Space: 17.9 MB (66.67%) - LeetHub

--- 4011-smallest-absent-positive-greater-than-average/4011-smallest-absent-positive-greater-than-average.py
+++ 4011-smallest-absent-positive-greater-than-average/4011-smallest-absent-positive-greater-than-average.py
@@ -4,8 +4,7 @@ class Solution:
     def smallestAbsent(self, nums: List[int]) -> int:
         avg = sum(nums) / len(nums)
         present = set(nums)
-        
-        candidate = int(avg) + 1  
+        candidate = max(1,int(avg) + 1)  
         while True:
             if candidate not in present:
                 return candidate
