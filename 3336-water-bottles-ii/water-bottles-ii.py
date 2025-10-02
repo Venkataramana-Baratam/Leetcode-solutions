b51c5a14018dc0a552fcea4ae9b4493df211841d
Venkataramana Baratam <venkataramanabaratam123@gmail.com>
2025-10-02 09:35:35 +0530

    Time: 40 ms (48.33%) | Memory: 17.9 MB (24.44%) - LeetSync

---
+++ 3336-water-bottles-ii/water-bottles-ii.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxBottlesDrunk(self, numBottles: int, numExchange: int) -> int:
+        total = numBottles
+        empty = numBottles
+        while empty>=numExchange:
+            empty-=numExchange
+            total+=1
+            empty+=1
+            numExchange+=1
+        return total
