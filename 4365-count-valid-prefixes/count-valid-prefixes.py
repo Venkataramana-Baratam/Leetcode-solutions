f6dc0e7e69904f79a48c80b2cb09d18b50563745
Venkataramana Baratam <venkataramanabaratam123@gmail.com>
2026-08-02 09:04:42 +0530

    Time: 1 ms (80.00%) | Memory: 19.3 MB (20.00%) - LeetSync

--- 4365-count-valid-prefixes/count-valid-prefixes.py
+++ 4365-count-valid-prefixes/count-valid-prefixes.py
@@ -1,26 +1,16 @@
 class Solution:
     def countValidPrefixes(self, s: str) -> int:
-        
+        zeros = 0
+        ones = 0
         ans = 0
 
-        i = 0 
-        j = i
-
-        while j < len(s):
-
-            zeros = 0
-            ones = 0
-
-            for ch in s[i:j+1]:
-
-                if ch == '0':
-                    zeros+=1
-                else:
-                    ones+=1
+        for ch in s:
+            if ch == '0':
+                zeros += 1
+            else:
+                ones += 1
 
             if abs(zeros - ones) <= 1:
+                ans += 1
 
-                ans+=1
-
-            j+=1
         return ans
